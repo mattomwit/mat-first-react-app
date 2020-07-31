a1b99888e89e5f3ff2de7b10f5c5ea5d0f7219a2
NamelessTactician <mattomwit@gmail.com>
2020-07-31 15:35:08 +0100

    Updates

---
+++ precache-manifest.08ef755cab998d3011d7833fe315ce57.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "236fbc5879432c7d85bf38ecaffb33e9",
+    "url": "/mat-first-react-app/index.html"
+  },
+  {
+    "revision": "879458c61f01af932dc9",
+    "url": "/mat-first-react-app/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "f5371f9090c363a11933",
+    "url": "/mat-first-react-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "879458c61f01af932dc9",
+    "url": "/mat-first-react-app/static/js/2.33f763e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mat-first-react-app/static/js/2.33f763e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5371f9090c363a11933",
+    "url": "/mat-first-react-app/static/js/main.b2c8aac1.chunk.js"
+  },
+  {
+    "revision": "d96d56dd04bb272ab546",
+    "url": "/mat-first-react-app/static/js/runtime-main.123d7668.js"
+  }
+]);
