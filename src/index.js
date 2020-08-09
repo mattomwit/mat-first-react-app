79dc3f4599940883f7ca492a3e71fe229a4a786e
NamelessTactician <mattomwit@gmail.com>
2020-08-09 13:07:32 +0100

    Added an incomplete navbar navbar

--- src/index.js
+++ src/index.js
@@ -7,12 +7,14 @@ import Game from './Components/Board/Game';
 import Home from './Components/Pages/Home';
 import Blog from './Components/Pages/Blog';
 import About from './Components/Pages/About';
+import Navbar from './Components/Navbar/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
-      
+    
+    <BrowserRouter basename="/first-react-app/">
+      <Navbar></Navbar>
       <Route path="/game" component={Game}></Route>
       <Route path="/home" component={Home}></Route>
       <Route path="/blog" component={Blog}></Route>
